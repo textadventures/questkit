5bf9de2b9b40a40c026e22059f381ad2539b166e
Alex Warren <alex@textadventures.co.uk>
2014-11-29 17:23:57 +0000

    Add sections to internal lists

--- questkit.js
+++ questkit.js
@@ -4,6 +4,7 @@ var questKitVersion = "0.0.1";
 
 var path = require("path");
 var fs = require("fs");
+var yaml = require("js-yaml");
 
 String.prototype.format = function() {
     var args = arguments;
@@ -18,20 +19,11 @@ String.prototype.format = function() {
 function Compiler() {
     this.process = function(inputFilename, sourcePath, options) {
         var outputPath = path.resolve(path.dirname(inputFilename));
-        var yaml = require("js-yaml");
 
-        var file = fs.readFileSync(inputFilename, "utf8");
         var sections = [];
 
-        try {
-            var docs = yaml.safeLoadAll(file, function(doc) {
-                sections.push(doc);
-            });
-        }
-        catch (e) {
-            console.log(e);
-            return;
-        }
+        this.processFile(sections, path.resolve(inputFilename));
+        this.processFile(sections, this.findFile("core.yaml", outputPath, sourcePath));
 
         console.log("Loaded {0} sections".format(sections.length));
         console.log("Writing story.js");
@@ -46,15 +38,26 @@ function Compiler() {
         var game = sections.shift();
 
         Object.keys(game).forEach(function(attr) {
-            outputJsFile.push("set(\"{0}\", {1});".format(attr, JSON.stringify(game[attr])));
+            outputJsFile.push("set(\"{0}\", {1});\n".format(attr, JSON.stringify(game[attr])));
         });
+
+        sections.forEach(function(section) {
+            this.writeSection(outputJsFile, section);
+        }, this);
 
         fs.writeFileSync(path.join(outputPath, "story.js"), outputJsFile.join(""));
 
         console.log("Done.");
 
         return outputPath;
-    }
+    };
+
+    this.processFile = function(sections, inputFilename) {
+        var file = fs.readFileSync(inputFilename, "utf8");
+        var docs = yaml.safeLoadAll(file, function(doc) {
+            sections.push(doc);
+        });
+    };
 
     this.findFile = function(filename, outputPath, sourcePath) {
         var outputPathFile = path.join(outputPath, filename);
@@ -62,7 +65,29 @@ function Compiler() {
             return outputPathFile;
         }
         return path.join(sourcePath, filename);
-    }
+    };
+
+    this.sectionTypes = {
+        "command": "commands",
+        "location": "objects",
+        "object": "objects",
+        "character": "objects",
+        "exit": "exits",
+    };
+
+    this.anonymousCount = 0;
+
+    this.writeSection = function(outputJsFile, section) {
+        var type = Object.keys(section)[0];
+        if (!(type in this.sectionTypes)) {
+            throw "Unknown type - {0}: {1}".format(type, section[type]);
+        }
+        
+        var name = section[type];
+        if (!name) name = "~" + this.anonymousCount++;
+
+        outputJsFile.push("Quest._internal.{0}.push(\"{1}\");\n".format(this.sectionTypes[type], name));
+    };
 }
 
 console.log("QuestKit " + questKitVersion);
