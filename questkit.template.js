f9c30f0b3b12d3f036d43e7f02e610f18571a7ac
Alex Warren <alex@textadventures.co.uk>
2014-11-30 17:56:17 +0000

    Convert some scope functions

--- questkit.template.js
+++ questkit.template.js
@@ -7,6 +7,9 @@ var Quest = {
 		exits: [],
 		regexes: {},
 	},
+	PovParent: function() {
+		return get(get("pov"), "parent");
+	},
 	HandleCommand: function(input) {
 		// TODO: Full conversion
 		Quest.ScopeCommands().forEach(function(cmd) {
@@ -23,7 +26,7 @@ var Quest = {
 						var check = args[index].toLowerCase();
 						var found = false;
 						// TODO: Use a Scope function instead
-						Quest._internal.objects.forEach(function(object) {
+						Quest.ScopeVisible().forEach(function(object) {
 							if (object.toLowerCase() == check) {
 								args[index] = object;
 								found = true;
@@ -42,7 +45,7 @@ var Quest = {
 					}
 					else {
 						// TODO: Get proper message from template
-						console.log("Not resolved");
+						msg("Not resolved");
 					}
 				}
 			});
@@ -50,11 +53,91 @@ var Quest = {
 	},
 	ScopeCommands: function() {
 		var result = [];
-		var povParent = get(get("pov"), "parent");
+		var povParent = Quest.PovParent();
 		Quest._internal.commands.forEach(function(cmd) {
 			var parent = get(cmd, "parent");
 			if (!parent || parent == povParent) {
 				result.push(cmd);
+			}
+		});
+		return result;
+	},
+	ScopeVisible: function() {
+		return Quest.ScopeVisibleNotHeld().concat(Quest.ScopeInventory());
+	},
+	ScopeVisibleNotHeld: function() {
+		return Quest.ScopeReachableNotHeldForLocation(Quest.PovParent()).concat(Quest.ScopeVisibleNotReachableForLocation(Quest.PovParent()));
+	},
+	ScopeReachableNotHeldForLocation: function(location) {
+		var result = [];
+		var pov = get("pov");
+		Quest.GetAllChildObjects(location).forEach(function(object) {
+			if (Quest.ContainsReachable(location, object) && object != pov && !Quest.Contains(pov, object)) {
+				result.push(object);
+			}
+		});
+		return result;
+	},
+	GetAllChildObjects: function(object) {
+		// TODO: Cache direct children of each object
+		var result = [];
+		Quest._internal.objects.forEach(function(child) {
+			if (get(child, "parent") == object) {
+				result.push(child);
+				result = result.concat(Quest.GetAllChildObjects(child));
+			}
+		});
+		return result;
+	},
+	ContainsVisible: function(parent, search) {
+		return Quest.ContainsAccessible(parent, search, false);
+	},
+	ContainsReachable: function(parent, search) {
+		return Quest.ContainsAccessible(parent, search, true);
+	},
+	ContainsAccessible: function(parent, search, onlyReachable) {
+		var searchParent = get(search, "parent");
+		if (!searchParent) return false;
+		if (get(search, "visible") == false) return false;
+		if (get(parent, "darklevel") && !get(search, "lightsource")) return false;
+		if (searchParent == parent) return true;
+		
+		var canAdd;
+		if (onlyReachable) {
+			canAdd = Quest.CanReachThrough(searchParent);
+		}
+		else {
+			canAdd = Quest.CanSeeThrough(searchParent);
+		}
+
+		if (canAdd) {
+			return (Quest.ContainsAccessible(parent, searchParent, onlyReachable));
+		} else {
+			return false;
+		}
+	},
+	CanSeeThrough: function(object) {
+		return (get(object, "transparent") || Quest.CanReachThrough(object)) && !get(object, "hidechildren");
+	},
+	CanReachThrough: function(object) {
+		return get(object, "isopen") && !get(object, "hidechildren");
+	},
+	Contains: function(parent, search) {
+		var searchParent = get(search, "parent");
+		if (!searchParent) return false;
+		if (searchParent == parent) return true;
+		return Quest.Contains(parent, searchParent);
+	},
+	ScopeVisibleNotReachableForLocation: function(location) {
+		// TODO
+		return [];
+	},
+	ScopeInventory: function() {
+		var result = [];
+		var pov = get("pov");
+		Quest.GetAllChildObjects(pov).forEach(function(object) {
+			if (Quest.ContainsVisible(pov, object)) {
+				result.push(object);
 			}
 		});
 		return result;
