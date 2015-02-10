5511461fcbc44ea489e5787ee0baac254bfc9664
Alex Warren <alex@textadventures.co.uk>
2015-02-10 19:16:53 +0000

    overflow-y: auto is better

--- ui.js
+++ ui.js
@@ -122,7 +122,7 @@ $.fn.questkit = function (options) {
 	});
 
 	if (settings.scroll === 'element') {
-		questkit.ui.output.css('overflow-y', 'scroll');
+		questkit.ui.output.css('overflow-y', 'auto');
 	}
 
 	questkit.ready();
